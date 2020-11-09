976775ef0d798f5321a6d7d482e0177358ce63a7
mengyaoss77 <solanin36@gmail.com>
2020-11-10 03:10:41 +0800

    服务器模式

---
+++ core/client.go
@@ -0,0 +1,121 @@
+package core
+
+import (
+	pb "go-space-chat/proto/star"
+	"html"
+	"log"
+	"time"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/gorilla/websocket"
+)
+
+type Client struct {
+	botId     string
+	dirty     bool
+	status    *pb.BotStatusRequest
+	msgs      []string
+	writeChan chan []byte
+	closeChan chan bool
+	conn      *websocket.Conn
+}
+
+func (c *Client) start() {
+	go c.waitAndWrite()
+	go c.readAndServe()
+}
+
+func (c *Client) close() {
+	close(c.closeChan)
+	c.conn.Close()
+}
+
+func (c *Client) readAndServe() {
+	defer func() {
+		c.close()
+	}()
+	for {
+		c.conn.SetReadDeadline(time.Second * 60)
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			log.Printf("read message error,client: %v break, ip: %v, err:%v", c.botId, c.conn.LocalAddr(), err)
+			messages <- &pb.BotStatusRequest{
+				BotId:  c.botId,
+				Status: pb.BotStatusRequest_close,
+			}
+			// 下线广播
+			if c.status != nil {
+				messages <- &pb.BotStatusRequest{
+					BotId: c.status.BotId,
+					Name:  "系统管理员",
+					Msg:   "用户@" + c.status.Name + " 下线了",
+				}
+			}
+			core.Clients.Delete(c.conn)
+			break
+		}
+		req := new(pb.BotStatusRequest)
+		err = proto.Unmarshal(message, req)
+		if err != nil {
+			log.Println(err)
+			break
+		}
+		if req.BotId != "" {
+			c.botId = req.BotId
+		}
+		// 文本过滤
+		req.Msg = core.TextSafer.Filter(req.Msg)
+		req.Name = core.TextSafer.Filter(req.Name)
+		req.Msg = html.EscapeString(req.Msg)
+		req.Name = html.EscapeString(req.Name)
+
+		if req.Msg != "" {
+			messages <- req
+			continue
+		}
+		if c.status == nil {
+			// 上线广播
+			messages <- &pb.BotStatusRequest{
+				BotId: req.BotId,
+				Name:  "系统管理员",
+				Msg:   "用户@" + req.Name + " 上线啦",
+			}
+		}
+		c.status = req
+		c.dirty = true
+	}
+}
+
+func (c *Client) waitAndWrite() {
+	for {
+		select {
+		case <-c.closeChan:
+			time.Sleep(time.Second)
+			close(c.writeChan)
+			return
+		case b, ok := <-c.writeChan:
+			if !ok { // channel 关闭了
+				return
+			}
+			err := c.conn.WriteMessage(websocket.BinaryMessage, b)
+			if err != nil {
+				log.Println("write err", err.Error())
+			}
+		}
+	}
+}
+
+func (c *Client) write(b []byte) {
+	defer func() {
+		// 防止写到关闭的writeChan里，但是做了延迟关闭 应该不会发生
+		r := recover()
+		if r != nil {
+			log.Println(r)
+		}
+	}()
+	select {
+	case <-c.closeChan:
+		return
+	case c.writeChan <- b:
+	}
+}
--- core/service.go
+++ core/service.go
@@ -4,10 +4,10 @@ import (
 	"flag"
 	"go-space-chat/component"
 	pb "go-space-chat/proto/star"
-	"html"
 	"log"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/gorilla/websocket"
@@ -20,6 +20,13 @@ type Core struct {
 	ConnMutex         sync.RWMutex
 	Clients           sync.Map // 客户端集合
 	TextSafer         component.TextSafe
+}
+
+var core *Core
+
+func InitCore() {
+	core = new(Core)
+	core.Run()
 }
 
 // 广播消息缓冲通道
@@ -51,6 +58,8 @@ func (c *Core) Run() {
 		}
 	}()
 
+	c.cronTask()
+
 	// 广播
 	go c.broadcast()
 	// pprof 性能
@@ -67,6 +76,21 @@ func (c *Core) Run() {
 	}
 }
 
+func (c *Core) cronTask() {
+	// 统计在线人数
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+			count := 0
+			c.Clients.Range(func(_, _ interface{}) bool {
+				count++
+				return true
+			})
+			log.Println("当前在线连接数: ", count)
+		}
+	}()
+}
+
 // 升级http为websocket协议
 func (c *Core) websocketUpgrade(w http.ResponseWriter, r *http.Request) {
 	// 跨域
@@ -79,110 +103,40 @@ func (c *Core) websocketUpgrade(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Printf("http upgrade webcoket err %v", err)
 	} else {
-		go c.listenWebsocket(conn)
-	}
-}
-
-// 监听message消息
-func (c *Core) listenWebsocket(conn *websocket.Conn) {
-	defer func() {
-		err := conn.Close()
-		if err != nil {
-			log.Printf("close websocket err %v", err)
+		var cli *Client
+		v, ok := c.Clients.Load(conn)
+		if !ok { // 创建一个新的客户端
+			cli = new(Client)
+			cli.conn = conn
+			cli.dirty = true
+			cli.writeChan = make(chan []byte, 64)
+			cli.closeChan = make(chan bool)
+			c.Clients.Store(conn, cli)
+		} else {
+			cli, _ = v.(*Client)
 		}
-	}()
-	// 监听
-	for {
-		// 尝试查询当前连接
-		cInfo, ok := c.Clients.Load(conn)
-		if !ok {
-			// 写入空
-			cInfo = &pb.BotStatusRequest{}
+		if cli != nil {
+			cli.start()
 		}
-
-		// 类型断言
-		clientInfo, ok := cInfo.(*pb.BotStatusRequest)
-		if !ok {
-			log.Printf("assert sync map pb.BotStatusRequest err %v", clientInfo)
-			c.Clients.Delete(conn)
-			continue
-		}
-		// 读取消息
-		_, message, err := conn.ReadMessage()
-		if err != nil {
-			log.Printf("read message error,client: %v break, ip: %v, err:%v", clientInfo.BotId, conn.LocalAddr(), err)
-			// 广播关闭连接
-			messages <- &pb.BotStatusRequest{
-				BotId:  clientInfo.BotId,
-				Status: pb.BotStatusRequest_close,
-			}
-			messages <- &pb.BotStatusRequest{
-				BotId: clientInfo.BotId,
-				Name:  "系统管理员",
-				Msg:   "用户@" + clientInfo.Name + " 下线了",
-			}
-			// 清除连接
-			c.Clients.Delete(conn)
-			err = conn.Close()
-			if err != nil {
-				log.Printf("close websocket err %v", err)
-			}
-			break
-		}
-		// 使用protobuf解析
-		pbr := &pb.BotStatusRequest{}
-		err = proto.Unmarshal(message, pbr)
-		if err != nil {
-			log.Printf("proto parse message %v err %v", message, err)
-			continue
-		}
-		// 敏感词过滤
-		pbr.Msg = c.TextSafer.Filter(pbr.Msg)
-		pbr.Name = c.TextSafer.Filter(pbr.Name)
-		// 过滤html 标签
-		pbr.Msg = html.EscapeString(pbr.Msg)
-		pbr.Name = html.EscapeString(pbr.Name)
-
-		// 初始化链接的ID
-		if clientInfo.BotId == "" {
-			c.Clients.Store(conn, &pb.BotStatusRequest{
-				BotId:  pbr.GetBotId(),
-				Name:   pbr.GetName(),
-				Status: pb.BotStatusRequest_connecting,
-			})
-			// 对新用户进行上线提示
-			pbr.Msg = "用户@" + pbr.Name + "  上线啦"
-			pbr.Name = "系统管理员"
-		}
-		// 广播队列
-		messages <- pbr
 	}
 }
 
 // 广播
 func (c *Core) broadcast() {
 	// 始终读取messages
-	for msg := range messages {
-		if msg.Msg != "" {
-			log.Printf("%s : %s", msg.BotId+":"+msg.Name, msg.Msg)
-		}
+	// 聊天消息通过这里广播
+	go func() {
+		for msg := range messages {
+			if msg.Msg != "" {
+				log.Printf("%s : %s", msg.BotId+":"+msg.Name, msg.Msg)
+			}
 
-		// 读取到之后进行广播，启动协程，是为了立即处理下一条msg
-		go func(m pb.BotStatusRequest) {
+			// 读取到之后进行广播，启动协程，是为了立即处理下一条msg
 			// 遍历所有客户
-			c.Clients.Range(func(connKey, bs interface{}) bool {
-
-				//bot, ok := bs.(*pb.BotStatusRequest)
-				//if !ok {
-				//	return true
-				//}
-				// 不给自己发消息
-				//if bot.BotId == m.BotId {
-				//	return true
-				//}
+			c.Clients.Range(func(connKey, value interface{}) bool {
 
 				resp := &pb.BotStatusResponse{
-					BotStatus: []*pb.BotStatusRequest{&m},
+					BotStatus: []*pb.BotStatusRequest{msg},
 				}
 				b, err := proto.Marshal(resp)
 				if err != nil {
@@ -190,21 +144,51 @@ func (c *Core) broadcast() {
 					return true
 				}
 
-				// 二进制发送
-				conn, ok := connKey.(*websocket.Conn)
+				cli, ok := value.(*Client)
 				if !ok {
-					log.Printf("assert connkey websocket.Conn err %v", conn)
 					return true
 				}
-				// 防止并发写
-				c.ConnMutex.Lock()
-				err = conn.WriteMessage(websocket.BinaryMessage, b)
-				c.ConnMutex.Unlock()
-				if err != nil {
-					log.Printf("conn write message err %v", err)
+				cli.write(b)
+				return true
+			})
+		}
+	}()
+
+	// 广播全服状态, 50ms一次
+	go func() {
+		for {
+			time.Sleep(time.Millisecond * 50)
+			resp := &pb.BotStatusResponse{}
+			// 遍历所有客户端状态
+			c.Clients.Range(func(connKey, value interface{}) bool {
+				cli, ok := value.(*Client)
+				if !ok {
+					return true
+				}
+				if !cli.dirty {
+					return true
+				}
+				if cli.status != nil {
+					resp.BotStatus = append(resp.BotStatus, cli.status)
+					cli.dirty = false
 				}
 				return true
 			})
-		}(*msg)
-	}
+
+			if len(resp.BotStatus) == 0 {
+				continue
+			}
+
+			b, _ := proto.Marshal(resp)
+
+			c.Clients.Range(func(connKey, value interface{}) bool {
+				cli, ok := value.(*Client)
+				if !ok {
+					return true
+				}
+				cli.write(b)
+				return true
+			})
+		}
+	}()
 }
